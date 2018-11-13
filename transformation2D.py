202694f93c5983ee9cfd628ec2229132091a953d
irfansofyana <irfansofyana0305@gmail.com>
2018-11-14 01:23:17 +0700

    add color for 2D, add rotate, stretch, and custom function

--- transformation2D.py
+++ transformation2D.py
@@ -1,3 +1,5 @@
+import math
+
 def translate(self, x, y):
 	for i in self.arrPoint:
 		i.x += x
@@ -26,3 +28,42 @@ def shear(self, a, k):
 		for i in self.arrPoint:
 			i.y += ((i.x-250)*k)
 	return self
+
+def rotate(self, deg, a, b):
+	deg = (2*deg*math.acos(-1))/360.0
+	for i in self.arrPoint:
+		i.x -= 250
+		i.x -= a
+		i.y -= 250
+		i.y -= b
+		tmpx = i.x * math.cos(deg) - i.y * math.sin(deg)
+		tmpy = i.x * math.sin(deg) + i.y * math.cos(deg)
+		i.x = tmpx + 250
+		i.y = tmpy + 250
+	return self
+
+def stretch(self, param, k):
+	if (param == 'x'):
+		for i in self.arrPoint:
+			i.x -= 250
+			i.x *= k
+			i.x += 250
+	else:
+		for i in self.arrPoint:
+			i.y -= 250
+			i.y *= k
+			i.y += 250
+	return self
+
+def custom (self, a, b, c, d):
+	for i in self.arrPoint:
+		i.x -= 250
+		i.y -= 250
+		t.x = i.x * a + i.y * b
+		t.y = i.x * c + i.y * d
+		i.x = t.x + 250
+		i.y = t.y + 250
+	return self
+
+def reset():
+	return
