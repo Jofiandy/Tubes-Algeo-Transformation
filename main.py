23918e5beda5e60d4f060a0e2083c39571a252dd
Jofiandy <jofiandyleonata@hotmail.com>
2018-11-18 05:03:02 +0700

    Add Function Buttton

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from transformation3D import *
 from random import *
 import sys
 import math
+import threading
 
 Wireframe = True
 width = 700
@@ -29,6 +30,17 @@ class Point:
 
 initial = Point(0,0,0)
 titik = Point(0,0,0)
+
+#2D was selected (calling all transformation and method for 2D)
+def run2d():
+    print("Masukan banyak titik: ")
+    n = int(input())
+    print("Masukan titik-titik tersebut:")
+    for i in range(0, n):
+        x,y = input().split()
+        titik.add_point(x,y,0)
+        initial.add_point(x,y,0)
+    makeWindow2D()
 
 #Draw Shape of the Blocks (So every changes be made, call this method)
 def drawShape2D(x):
@@ -73,40 +85,197 @@ def draw2D():
                 comm = input()
                 doCommand2D(comm)
 
+#do the command for 2d only
+def doCommand2D(comm):
+    if (comm == "translate"):
+        x, y = input().split()
+        P = translate(titik, float(x), float(y))
+    elif (comm == "dilate"):
+        x = input()
+        P = dilate(titik, float(x))
+    elif (comm == "reflect"):
+        x, y = input().split()
+        P = reflect(titik, float(x), float(y))
+    elif (comm == "shear"):
+        a, k = input().split()
+        P = shear(titik, a, float(k))
+    elif (comm == "rotate"):
+        deg, a, b = input().split()
+        P = rotate(titik, float(deg), float(a), float(b))
+    elif (comm == "stretch"):
+        param, x = input().split()
+        P = stretch(titik, param, float(x))
+    elif (comm == "custom"):
+        a,b,c,d = input().split()
+        P = custom(titik, float(a), float(b), float(c), float(d))
+    elif (comm == "reset"):
+        P = reset(titik, initial)
+
+
+#Make the window of the program
+def makeWindow2D():
+    glutInit()                                             # initialize glut
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+    glutInitWindowSize(width, height)                      # set window size
+    glutInitWindowPosition(0, 0)                           # set window position
+    window = glutCreateWindow("Tubes Algeo 2D")              # create window with title
+    glutDisplayFunc(draw2D)                                  # set draw function callback
+    glutIdleFunc(draw2D)                                     # draw all the time
+    glutMainLoop()
+
+#Buffer the drawing for 2D
+def refresh2d(width, height):
+    glViewport(0,0 , width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-width, width, -height, height, 0.0, 1.0)
+    glMatrixMode (GL_MODELVIEW)
+    glLoadIdentity()
+
+#3D was selected (calling all transformation and method for 3D)
+def run3d():
+    tmpArr = [(-0.5, -0.5, 0.5),
+    (0.5, -0.5, 0.5), (0.5, 0.5, 0.5), (-0.5, 0.5, 0.5),
+    (-0.5, -0.5, -0.5),(-0.5, 0.5, -0.5), ( 0.5, 0.5, -0.5),( 0.5, -0.5, -0.5), (-0.5, -0.5, 0.5)
+    ,(-0.5, 0.5, 0.5), (-0.5, 0.5, -0.5), (-0.5, -0.5, -0.5), ( 0.5, -0.5, -0.5), ( 0.5, 0.5, -0.5),
+    ( 0.5, 0.5, 0.5), ( 0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), ( 0.5, 0.5, 0.5), ( 0.5, 0.5, -0.5),
+    (-0.5, 0.5, -0.5), (-0.5, -0.5, 0.5), (-0.5, -0.5, -0.5),( 0.5, -0.5, -0.5),( 0.5, -0.5, 0.5)]
+    for i in tmpArr:
+        titik.add_point(i[0], i[1], i[2])
+        initial.add_point(i[0], i[1], i[2])
+    makeWindow3D()
+    return
+
+def makeWindow3D():
+    global window
+    glutInit()                                             # initialize glut
+    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
+    glutInitWindowSize(width, height)                      # set window size
+    glutInitWindowPosition(0, 0)
+    window = glutCreateWindow("Tubes Algeo 3D")
+    glutDisplayFunc(draw3D)
+    glutIdleFunc(draw3D)
+    glutReshapeFunc(ReSizeGLScene)
+    InitGL(width, height)
+    glutMainLoop()
+
+def InitGL(Width, Height):
+    glClearColor(0.0, 0.0, 0.0, 0.0)
+    glClearDepth(1.0)
+    glDepthFunc(GL_LESS)
+    glEnable(GL_DEPTH_TEST)
+    glPolygonMode(GL_FRONT, GL_LINE)
+    glPolygonMode(GL_BACK, GL_LINE)
+    glShadeModel(GL_SMOOTH)
+
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+
+    glMatrixMode(GL_MODELVIEW)
+
+def ReSizeGLScene(Width, Height):
+    if Height == 0:
+        Height = 1
+    glViewport(0, 0, Width, Height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
+    glMatrixMode(GL_MODELVIEW)
+
 def draw3D():
-    while (True):
-        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
-        glMatrixMode(GL_MODELVIEW)
+    # print(initial.i)
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # clear the screen
+    glMatrixMode(GL_MODELVIEW)
 
-        glLoadIdentity()
+    glLoadIdentity()
 
-        gluLookAt(
-        0.0, 0.0, 3.0,
-        0.0, 0.0, 0.0,
-        0.0, 1.0, 0.0)
+    gluLookAt(
+    0.0, 0.0, 3.0,
+    0.0, 0.0, 0.0,
+    0.0, 1.0, 0.0)
 
-        glTranslatef(0.0, 0.0, -5.0)
+    glutKeyboardFunc(keyPressed)
+    glTranslatef(0.0, 0.0, -5.0)
 
-        glRotatef(x_rot, 1.0, 0.0, 0.0)
-        glRotatef(y_rot, 0.0, 1.0, 0.0)
-        glTranslatef(tra_x, tra_y, tra_z)
+    glRotatef(x_rot, 1.0, 0.0, 0.0)
+    glRotatef(y_rot, 0.0, 1.0, 0.0)
+    glTranslatef(tra_x, tra_y, tra_z)
 
-        drawShape3D(titik)
-        glFlush()
-        glutSwapBuffers()
+    drawShape3D(titik)
+    glFlush()
+    glutSwapBuffers()
 
-        comm = input()
-        if (comm == "dilate"):
-            n = input()
-            P = dilate3D(titik, float(n))
-        elif (comm == "exit"):
-            sys.exit()
+def doCommand3D():
+    comm = input()
+    if (comm == "translate"):
+        x, y, z = input().split()
+        P = translate3D(titik, float(x), float(y), float(z))
+    elif (comm == "dilate"):
+        n = input()
+        P = dilate3D(titik, float(n))
+    elif (comm == "reflect"):
+        x, y, z = input().split()
+        P = reflect3D(titik, float(x), float(y), float(z))
+    elif (comm == "shear"):
+        a, k = input().split()
+        P = shear(titik, a, float(k))
+    elif (comm == "rotate"):
+        deg, a, b = input().split()
+        P = rotate(titik, float(deg), float(a), float(b))
+    elif (comm == "stretch"):
+        param, x = input().split()
+        P = stretch3D(titik, param, float(x))
+    elif (comm == "custom"):
+        a, b, c, d, e, f, g, h, i = input().split()
+        P = custom3D(titik, float(a), float(b), float(c), float(d))
+    elif (comm == "reset"):
+        P = reset3D(titik, initial)
+    elif (comm == "exit"):
+        sys.exit()
+
+def keyPressed(*args):
+    global tra_x
+    global tra_y
+    global tra_z
+    global x_rot
+    global y_rot
+    if (args[0] == b"w"):
+        tra_z -= 0.1
+    elif (args[0] == b"s"):
+        tra_z += 0.1
+    elif (args[0] == b"a"):
+        tra_x += 0.1
+    elif (args[0] == b"d"):
+        tra_x -= 0.1
+    elif (args[0] == b"z"):
+        tra_y += 0.1
+    elif (args[0] == b"c"):
+        tra_y -= 0.1
+    elif (args[0] == b"u"):
+        x_rot += 2.0
+        y_rot += 2.0
+    elif (args[0] == b"y"):
+        x_rot -= 2.0
+        y_rot -= 2.0
+    elif (args[0] == b"x"):
+        global Wireframe
+        if Wireframe==False:
+            glPolygonMode(GL_FRONT, GL_LINE)
+            glPolygonMode(GL_BACK, GL_LINE)
+            Wireframe=True
+        elif Wireframe ==True:
+            glPolygonMode(GL_FRONT, GL_FILL)
+            glPolygonMode(GL_BACK, GL_FILL)
+            Wireframe=False
+    elif(args[0] == b"f"):
+        doCommand3D()
 
 def drawShape3D(x):
     glBegin(GL_QUADS)
-    
     glColor3f(1.0, 0.0,  0.0)
-    
+
     #front
     glVertex3f(x.arrPoint[0].x, x.arrPoint[0].y, x.arrPoint[0].z)
     glColor3f(0.0, 1.0, 0.0)
@@ -114,7 +283,7 @@ def drawShape3D(x):
     glVertex3f(x.arrPoint[2].x, x.arrPoint[2].y, x.arrPoint[2].z)
     glColor3f(1.0, 0.0, 0.0)
     glVertex3f(x.arrPoint[3].x, x.arrPoint[3].y, x.arrPoint[3].z)
-    
+
     #back
     glVertex3f(x.arrPoint[4].x, x.arrPoint[4].y, x.arrPoint[4].z)
     glVertex3f(x.arrPoint[5].x, x.arrPoint[5].y, x.arrPoint[5].z)
@@ -123,7 +292,7 @@ def drawShape3D(x):
     glVertex3f(x.arrPoint[7].x, x.arrPoint[7].y, x.arrPoint[7].z)
 
     glColor3f(0.0, 1.0, 0.0)
-    
+
     #left
     glVertex3f(x.arrPoint[8].x, x.arrPoint[8].y, x.arrPoint[8].z)
     glVertex3f(x.arrPoint[9].x, x.arrPoint[9].y, x.arrPoint[9].z)
@@ -158,150 +327,6 @@ def drawShape3D(x):
     glVertex3f(x.arrPoint[23].x, x.arrPoint[23].y, x.arrPoint[23].z)
 
     glEnd()
-
-#do the command for 2d only
-def doCommand2D(comm):
-    if (comm == "translate"):
-        x, y = input().split()
-        P = translate(titik, float(x), float(y))
-    elif (comm == "dilate"):
-        x = input()
-        P = dilate(titik, float(x))
-    elif (comm == "reflect"):
-        x, y = input().split()
-        P = reflect(titik, float(x), float(y))
-    elif (comm == "shear"):
-        a, k = input().split()
-        P = shear(titik, a, float(k))
-    elif (comm == "rotate"):
-        deg, a, b = input().split()
-        P = rotate(titik, float(deg), float(a), float(b))
-    elif (comm == "stretch"):
-        param, x = input().split()
-        P = stretch(titik, param, float(x))
-    elif (comm == "custom"):
-        a,b,c,d = input().split()
-        P = custom(titik, float(a), float(b), float(c), float(d))
-    elif (comm == "reset"):
-        P = reset(titik, initial)
-
-
-def keyPressed(*args):
-    global tra_x
-    global tra_y
-    global tra_z
-    global x_rot
-    global y_rot
-    if (args[0] == b"w"):
-        tra_z -= 0.1
-    elif (args[0] == b"s"):
-        tra_z += 0.1
-    elif (args[0] == b"a"):
-        tra_x += 0.1
-    elif (args[0] == b"d"):
-        tra_x -= 0.1
-    elif (args[0] == b"u"):
-        x_rot += 2.0
-        y_rot += 2.0 
-    elif (args[0] == b"y"):
-        x_rot -= 2.0
-        y_rot -= 2.0
-    elif (args[0] == b"x"):
-        global Wireframe
-        if Wireframe==False:
-            glPolygonMode(GL_FRONT, GL_LINE)    
-            glPolygonMode(GL_BACK, GL_LINE)
-            Wireframe=True
-        elif Wireframe ==True:
-            glPolygonMode(GL_FRONT, GL_FILL)
-            glPolygonMode(GL_BACK, GL_FILL)
-            Wireframe=False
-    # print(args[0])
-    # glutPostRedisplay()
-
-#Make the window of the program
-def makeWindow2D():
-    glutInit()                                             # initialize glut
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(width, height)                      # set window size
-    glutInitWindowPosition(0, 0)                           # set window position
-    window = glutCreateWindow("Tubes Algeo 2D")              # create window with title
-    glutDisplayFunc(draw2D)                                  # set draw function callback
-    glutIdleFunc(draw2D)                                     # draw all the time
-    glutMainLoop()
-
-def makeWindow3D():
-    global window
-    glutInit()                                             # initialize glut
-    glutInitDisplayMode(GLUT_RGBA | GLUT_DOUBLE | GLUT_ALPHA | GLUT_DEPTH)
-    glutInitWindowSize(width, height)                      # set window size
-    glutInitWindowPosition(0, 0)
-    window = glutCreateWindow("Tubes Algeo 3D")
-    glutDisplayFunc(draw3D)
-    glutIdleFunc(draw3D)
-    glutReshapeFunc(ReSizeGLScene)
-    glutKeyboardFunc(keyPressed)
-    InitGL(width, height)
-    glutMainLoop()
-
-def InitGL(Width, Height):                
-    glClearColor(0.0, 0.0, 0.0, 0.0)    
-    glClearDepth(1.0)                   
-    glDepthFunc(GL_LESS)                
-    glEnable(GL_DEPTH_TEST)
-    glPolygonMode(GL_FRONT, GL_LINE)    
-    glPolygonMode(GL_BACK, GL_LINE)     
-    glShadeModel(GL_SMOOTH)                
-
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()                    
-                                        
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-
-    glMatrixMode(GL_MODELVIEW)
-
-def ReSizeGLScene(Width, Height):
-    if Height == 0:                       
-        Height = 1
-    glViewport(0, 0, Width, Height)        
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    gluPerspective(45.0, float(Width)/float(Height), 0.1, 100.0)
-    glMatrixMode(GL_MODELVIEW)
-
-#Buffer the drawing
-def refresh2d(width, height):
-    glViewport(0,0 , width, height)
-    glMatrixMode(GL_PROJECTION)
-    glLoadIdentity()
-    glOrtho(-width, width, -height, height, 0.0, 1.0)
-    glMatrixMode (GL_MODELVIEW)
-    glLoadIdentity()
-
-#2D was selected (calling all transformation and method for 2D)
-def run2d():
-    print("Masukan banyak titik: ")
-    n = int(input())
-    print("Masukan titik-titik tersebut:")
-    for i in range(0, n):
-        x,y = input().split()
-        titik.add_point(x,y,0)
-        initial.add_point(x,y,0)
-    makeWindow2D()
-
-#3D was selected (calling all transformation and method for 3D)
-def run3d():
-    tmpArr = [(-0.5, -0.5, 0.5), 
-    (0.5, -0.5, 0.5), (0.5, 0.5, 0.5), (-0.5, 0.5, 0.5), 
-    (-0.5, -0.5, -0.5),(-0.5, 0.5, -0.5), ( 0.5, 0.5, -0.5),( 0.5, -0.5, -0.5), (-0.5, -0.5, 0.5)
-    ,(-0.5, 0.5, 0.5), (-0.5, 0.5, -0.5), (-0.5, -0.5, -0.5), ( 0.5, -0.5, -0.5), ( 0.5, 0.5, -0.5), 
-    ( 0.5, 0.5, 0.5), ( 0.5, -0.5, 0.5), (-0.5, 0.5, 0.5), ( 0.5, 0.5, 0.5), ( 0.5, 0.5, -0.5), 
-    (-0.5, 0.5, -0.5), (-0.5, -0.5, 0.5), (-0.5, -0.5, -0.5),( 0.5, -0.5, -0.5),( 0.5, -0.5, 0.5)]
-    for i in tmpArr:
-        titik.add_point(i[0], i[1], i[2])
-        initial.add_point(i[0], i[1], i[2])
-    makeWindow3D()
-    return
 
 #Main program (include making the windows)
 print("Selamat datang pada program simulasi transformasi linier")
--- transformation3D.py
+++ transformation3D.py
@@ -11,7 +11,7 @@ def dilate3D(self, k):
 		i.y *= k
 		i.z *= k
 	return self
-	
+
 def reflect3D(self, x, y, z):
 	for i in self.arrPoint:
 		tmp = i.x - x
@@ -21,19 +21,28 @@ def reflect3D(self, x, y, z):
 		tmp = i.z - z
 		i.z -= 2*tmp
 	return self
-	
-def stretch3D(self, param, k):
+
+def shear3D(self, a, k): #problem
+	if (a=='x'):
+		for i in self.arrPoint:
+			i.x += i.y*k
+	elif (a=='y'):
+		for i in self.arrPoint:
+			i.y += i.x*k
+	return self
+
+def stretch3D(self, param, k): #problem
 	if (param == 'x'):
 		for i in self.arrPoint:
 			i.x = ((i.x-250)*k * 250)
 	elif (param == 'y'):
 		for i in self.arrPoint:
 			i.y = ((i.y-250)*k * 250)
-		else:
-			for i in self.arrPoint:
-				i.z = ((i.z-250)*k * 250)
+	elif (param == 'z'):
+		for i in self.arrPoint:
+			i.z = ((i.z-250)*k * 250)
 	return self
-	
+
 def custom3D (self, a, b, c, d, e, f, g, h, i):
 	for i in self.arrPoint:
 		t.x = (i.x - 250) * a + (i.y - 250) * d + (i.z - 250) * g
@@ -42,4 +51,11 @@ def custom3D (self, a, b, c, d, e, f, g, h, i):
 		i.x = t.x + 250
 		i.y = t.y + 250
 		i.z = t.z + 250
+	return self
+
+def reset3D(self, point): #problem
+	for i, j in zip(self.arrPoint, point.arrPoint):
+		i.x = j.x
+		i.y = j.y
+		i.z = j.z
 	return self
